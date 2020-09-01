cf4da24051f54c51e04e2a18ad8e3836314aa703
jeremy <jeremy@niubi.com>
2020-09-01 18:55:56 +0800

    分支提交测试

---
+++ src/main/java/com/wang/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.wang.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaa");
+    }
+}
