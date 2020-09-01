0e762ea4d97e152cd89e88614a8e5e0467d8c2eb
jeremy <jeremy@niubi.com>
2020-09-01 18:57:55 +0800

    测试提交冲突 master

--- src/main/java/com/wang/git/GitTest.java
+++ src/main/java/com/wang/git/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaa");
         System.out.println("aaaaaaaaaaa");
-        System.out.println("aaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaa");//edit by master
         System.out.println("aaaaaaaaaaa");
         System.out.println("aaaaaaaaaaa");
         System.out.println("aaaaaaaaaaa");
