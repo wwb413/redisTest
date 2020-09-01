960c4ffb0786003c586d8d7b67b4003ec0007e33
jeremy <jeremy@niubi.com>
2020-09-01 18:59:38 +0800

    测试提交冲突 hotfix

--- src/main/java/com/wang/git/GitTest.java
+++ src/main/java/com/wang/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaa");
         System.out.println("aaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaa");//edit by hotfix
         System.out.println("aaaaaaaaaaa");//edit by master
         System.out.println("aaaaaaaaaaa");
         System.out.println("aaaaaaaaaaa");
